a2970fe06a7295d5750462f567905fa322f76706
Mayank Yadav <xiatio08@gmail.com>
2022-01-23 11:13:17 +0530

    initialised website files

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import Form from "./component/Form/Form";
+import Posts from "./component/Posts/Posts";
+import memories from "./images/memories.png";
+
+const App = () => {
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid items xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid items xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
